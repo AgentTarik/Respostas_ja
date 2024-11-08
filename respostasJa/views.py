05f68c178a4741969ad4c94256fe03c9a8cf7d14
carolinadfb <caroldefalcob@gmail.com>
2024-11-08 18:10:58 +0000

    login

--- respostasJa/views.py
+++ respostasJa/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def logindapag (request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print (f'Usuario:{username}, Senha:{password}')
+    return render (request,'login.html')
+    
