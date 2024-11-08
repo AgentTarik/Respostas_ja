030b0fe836869a75689d804bb6c99ed40fd7832e
Bruno Rêgo <91488118+AgentTarik@users.noreply.github.com>
2024-11-08 16:15:17 +0000

    mudancas em models

--- respostasJa/models.py
+++ respostasJa/models.py
@@ -19,10 +19,23 @@ class Formulario(models.Model):
 class Campo(models.Model):
     formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE, related_name='campos')
     tipo = models.CharField(max_length=50)
-    pergunta = models.CharField(max_length=255)godels.CASCADE, related_name='respostas')
+    pergunta = models.CharField(max_length=255, on_delete=models.CASCADE, related_name='respostas')
+
+    def __str__(self):
+        return f"{Formulario.idFormulario} {self.campo.pergunta}"
+
+class RespostaFormulario(models.Model):
+    formulario = models.ForeignKey(Formulario, on_delete=models.CASCADE, related_name='respostas_formulario')
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, blank=True, related_name='respostas_formulario')
+    data_resposta = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Respostas para {self.formulario.titulo} por {self.usuario.idUsuario if self.usuario else 'Usuário Anônimo'}"
+
+class RespostaCampo(models.Model):
+    resposta_formulario = models.ForeignKey(RespostaFormulario, on_delete=models.CASCADE, related_name='respostas_campos')
     campo = models.ForeignKey(Campo, on_delete=models.CASCADE, related_name='respostas')
-    idResposta = models.AutoField(primary_key=True)
     conteudo = models.TextField()
 
     def __str__(self):
-        return f"Resposta {self.idResposta} para {self.campo.pergunta}"
+        return f"Resposta para '{self.campo.pergunta}': {self.conteudo}"
